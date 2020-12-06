e330f774851af6a4cdba290bdbb15de87b3d93aa
Justin Hinckley <justin.h.hinckley@gmail.com>
2020-12-06 15:56:33 -0500

    made notifications disappear when app opens

--- android/app/src/main/kotlin/com/justinhinckley/gt_scheduling_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/justinhinckley/gt_scheduling_app/MainActivity.kt
@@ -2,5 +2,18 @@ package com.justinhinckley.gt_scheduling_app
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.app.NotificationManager
+import android.content.Context
+
 class MainActivity: FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        // Removing All Notifications
+        cancelAllNotifications()
+    }
+
+    private fun cancelAllNotifications() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+    }
 }
